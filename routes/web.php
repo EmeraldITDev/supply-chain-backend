34719c40895421a5c1b26acdce6a1c62e6f77aac
asuku <asukuonukaba@gmail.com>
2026-01-28 10:44:41 +0100

    refactor: Remove unused storage test route from web.php
    - Deleted the '/storage-test' route that was previously used for debugging filesystem configuration settings, streamlining the routing file.

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/storage-test', function () {
-    dd(
-        config('filesystems.default'),
-        config('filesystems.documents_disk')
-    );
-});
