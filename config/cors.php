2aaf0d354f369d62a872e8433986be39651739eb
asuku <asukuonukaba@gmail.com>
2026-01-28 14:46:32 +0100

    feat: Add new CORS origin for SCM subdomain
    - Included 'https://scm.emeraldcfze.com/' in the CORS configuration to allow requests from the SCM subdomain, enhancing cross-origin resource sharing capabilities.

--- config/cors.php
+++ config/cors.php
@@ -27,6 +27,7 @@ return [
             'http://localhost:3000',
             'http://localhost:5173',
             'https://emerald-supply-chain.vercel.app',
+            'https://scm.emeraldcfze.com/',
             'https://*.vercel.app',
         ],
 
