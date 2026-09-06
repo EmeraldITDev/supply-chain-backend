a9db51dd3464e324432eca407fe77dab527884b1
AsukuOnukaba <asukuonukaba@gmail.com>
2026-09-06 21:26:45 +0100

    feat: update database configuration to use environment variable for SSL mode and add PDO options

--- config/database.php
+++ config/database.php
@@ -95,7 +95,11 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'require'),
+            'options' => extension_loaded('pdo_pgsql') ? array_filter([
+                PDO::ATTR_PERSISTENT => false,
+                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
+            ]) : [],
         ],
 
         'sqlsrv' => [
