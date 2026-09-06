02c445a2cb3fdfdcd78de07b259d7b2cbfd3dfbb
AsukuOnukaba <asukuonukaba@gmail.com>
2026-09-06 21:30:19 +0100

    fix: change SSL mode in database configuration from environment variable to 'prefer'

--- config/database.php
+++ config/database.php
@@ -95,11 +95,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'require'),
-            'options' => extension_loaded('pdo_pgsql') ? array_filter([
-                PDO::ATTR_PERSISTENT => false,
-                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
-            ]) : [],
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
